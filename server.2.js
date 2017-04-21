d943d22f41947bc836436d1565c384b60ee6260d
John C <John@Johns-MacBook-Air.local>
2017-04-21 16:57:41 -0700

    added clusters on servers

--- server.2.js
+++ server.2.js
@@ -2,17 +2,43 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const httpProxy = require('http-proxy');
+const cluster = require('cluster');
 
-http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/html') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-      res.end(data);
-    });
+if (cluster.isMaster) { console.log('cluster is master');
+  const numWorkers = require('os').cpus().length/2;
+
+  console.log('Master cluster setting up ' + numWorkers + ' workers...');
+
+  for (let i = 0; i < numWorkers; i += 1) {
+    cluster.fork();
   }
-  if (req.method === 'GET' && req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('LOOK AT ME\n Server 4000');
-  }
-}).listen(4000, '127.0.0.1');
-console.log('Server running at 127.0.0.1:4000');
+
+  cluster.on('online', function (worker) {
+    console.log('Worker ' + worker.process.pid + ' is online');
+  });
+
+  cluster.on('exit', function (worker, code, signal) {
+    console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+    console.log('Starting a new worker');
+    cluster.fork();
+  });
+} else {
+  http.createServer((req, res) => {
+
+    console.log('pid', process.pid, 'handler start, blocking CPU');
+
+    console.log('pid', process.pid, 'unblocked, responding');
+
+    if (req.method === 'GET' && req.url === '/html') {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+        res.end(data);
+      });
+    }
+    if (req.method === 'GET' && req.url === '/') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('LOOK AT ME\n Server 4000');
+    }
+  }).listen(4000, '127.0.0.1');
+  console.log('Server running at 127.0.0.1:4000');
+}
