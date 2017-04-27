529c17950ff772a19ef5f9688f0dd17f3264b2e6
John C <John@Johns-MacBook-Air.local>
2017-04-26 20:24:16 -0700

    read and write json

--- testServer/serverCluster.js
+++ testServer/serverCluster.js
@@ -13,25 +13,81 @@ if (cluster.isMaster) { console.log('cluster is master');
   for (let i = 0; i < numWorkers; i += 1) {
     cluster.fork();
   }
-  // let the user know the id of the thread worker
-  cluster.on('online', function (worker) {
+
+  cluster.on('online', (worker) => {
+    // let user know thread is online
     console.log('Worker ' + worker.process.pid + ' is online');
+    // open clusterRequests.json so we can store the threadID, and save it as 0
+    // the value is 0 because no requests were made to this thread yet
+    fs.readFile(path.join(__dirname, '../clusterRequests.json'), (err, data) => {
+      if (err) throw err;
+      let config = JSON.parse(data);
+
+      // because of nodemon constantly restarting for whenever the json file is
+      // changed, here is an if statement to make it a new empty array for when
+      // its length equals the number of threads on the user's CPU
+      if (config.length >= numWorkers) config = [];
+      const obj = {};
+      obj[worker.process.pid] = 0;
+      config.push(obj);
+      const configJSON = JSON.stringify(config);
+      fs.writeFile(path.join(__dirname, '../clusterRequests.json'), configJSON, (err) => {
+        if (err) throw err;
+      });
+    });
   });
 
   // when a worker dies executing code, create another
-  cluster.on('exit', function (worker, code, signal) {
+  cluster.on('exit', (worker, code, signal) => {
     console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
     console.log('Starting a new worker');
     cluster.fork();
+    // fs.readFile(path.join(__dirname, '../clusterRequests.json'), (err, data) => {
+    //   if (err) throw err;
+    //
+    //   const con = JSON.parse(data);
+    //
+    //   const threadToChange = con.findIndex(e => e.hasOwnProperty(process.pid.toString()));
+    //
+    //   con.splice(threadToChange, 1);
+    //   const conJSON = JSON.stringify(con);
+    //   fs.writeFile(path.join(__dirname, '../clusterRequests.json'), conJSON, (err) => {
+    //     if (err) throw err;
+    //   });
+    // });
   });
 } else {
   // create a server
   http.createServer((req, res) => {
     console.log(`worker ${process.pid} is working`);
+
+    // open clusterRequests file to find the thread property, and then
+    // add 1 to its value since a request has been made to that thread
+    fs.readFile(path.join(__dirname, '../clusterRequests.json'), (err, data) => {
+      if (err) throw err;
+
+      const con = JSON.parse(data);
+      const threadToChange = con.findIndex(e => e.hasOwnProperty(process.pid.toString()));
+
+      if (threadToChange > -1) { console.log(`worker ${process.pid} passed if statement`)
+        con[threadToChange][process.pid.toString()]++;
+        fs.writeFile(path.join(__dirname, '../clusterRequests.json'), JSON.stringify(con), (err) => {
+          if (err) throw err;
+        });
+      } else {
+        const newObj = {};
+        newObj[process.pid] = 1;
+        con.push(newObj);
+        fs.writeFile(path.join(__dirname, '../clusterRequests.json'), JSON.stringify(con), (err) => {
+          if (err) throw err;
+        });
+      }
+    });
     // send html file on a get request
     if (req.method === 'GET' && req.url === '/html') {
       res.writeHead(200, { 'Content-Type': 'text/html' });
-      fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+      fs.readFile(path.join(__dirname, '../index.html'), (err, data) => {
+        console.log(data);
         res.end(data);
       });
     }
