eed04fac4879c1efa7b1bf284c40033c6e7e9b0b
Will Rigsbee <willrigsbee@Users-MacBook-Pro.local>
2017-04-26 21:13:21 -0700

    full cycle session save and retrieval with redis

--- testServer/server.js
+++ testServer/server.js
@@ -7,7 +7,7 @@ const port = process.argv[2];
 console.log(port);
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  // console.log(req.headers.cookie);
   if (req.method === 'GET' && req.url === '/html') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     fs.readFile(path.join(__dirname, '..', 'index.html'), (err, data) => {
@@ -31,6 +31,11 @@ const server = http.createServer((req, res) => {
     authController.login(req, res);
   }
 
+  if (req.method === 'GET' && req.url === '/verifyUser') {
+    authController.verifyUser(req, (isVerified) => {
+      res.end(JSON.stringify(isVerified));
+    });
+  }
 }).listen(port); // ex ec2-52-53-200-5.us-west-1.compute.amazonaws.com
 console.log('Server running at port ' + port);
 
