244eebe8423c320210c4a06dac8f91fa31b5f555
John C <John@Johns-MacBook-Air.local>
2017-05-08 20:22:51 -0700

    added https to server variable and secureOpts obj

---
+++ testServer/secureServer.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const lb = require('../serverLb/library/nodelb');
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  host: '127.0.0.1',
+  port: 6379,
+};
+
+const rs = lb.deploy('redis', options);
+const threads = lb.deploy('threads');
+
+const port = process.argv[2];
+
+const secureOpts = {
+    key: fs.readFileSync('server-key.pem'),
+    cert: fs.readFileSync('server-crt.pem'),
+    ca: fs.readFileSync('ca-crt.pem'),
+};
+
+// https.createServer(options, (res) => {
+//   console.log('went into https server');
+//   res.writeHead(200);
+//   res.end('you got a secured connection!');
+// }).listen(4433, () => console.log('listening on port 4433'));
+
+const server = https.createServer(secureOpts, (req, res) => {
+  // console.log(req.headers.cookie);
+  if (req.method === 'GET' && req.url === '/html') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile(path.join(__dirname, '..', 'index.html'), (err, data) => {
+      res.end(data);
+    });
+  }
+
+  if (req.method === 'GET' && req.url === '/') {
+    console.log('server hit');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('LOOK AT ME\n Server ' + port, () => console.log('response completed from port ' + port));
+  }
+
+  if (req.method === 'GET' && req.url === '/bundle.js') {
+    res.writeHead(200, { 'Content-Type': 'application/javascript' });
+    fs.readFile(path.join(__dirname, '..', 'testClient', 'bundle.js'), (err, data) => {
+      res.end(data);
+    });
+  }
+
+  if (req.method === 'GET' && req.url === '/server.jpg') {
+    res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+    fs.readFile(path.join(__dirname, '..', 'testClient', 'img', 'server.jpg'), (err, data) => {
+      res.end(data);
+    });
+  }
+
+  if (req.method === 'POST' && req.url === '/login') {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk;
+    }).on('end', () => {
+      body = JSON.parse(body);
+      if (body.username === 'yo' && body.password === 'yo') {
+        rs.authenticate(req, res, 'SID', body.username, (err, reply) => {
+          console.log(reply);
+          res.end('true');
+        });
+      } else res.end('false');
+    });
+  }
+  if (req.method === 'GET' && req.url === '/verifyUser') {
+    rs.verifySession(req, 'SID', (isVerified) => {
+      res.end(JSON.stringify(isVerified));
+    });
+  }
+});
+
+threads(server, port);
