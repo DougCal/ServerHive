35145bd24e29e729aaeca9e194d14ab90aa3aa8a
Darrick Mckirnan <dtmckirnan@Darricks-MacBook-Pro.local>
2017-05-12 11:03:58 -0700

    updated and refactored some code

--- serverLb/library/throttleIP.js
+++ serverLb/library/throttleIP.js
@@ -1,12 +1,9 @@
 const ipAddresses = {};
 
 const ddosStopper = (bReq, bRes, delay) => {
-
-  const ip = (bReq.headers['x-forwarded-for'] || '').split(',')[0]
-    || bReq.connection.remoteAddress;
+  const ip = (bReq.headers['x-forwarded-for'] || '').split(',')[0] || bReq.connection.remoteAddress;
   // if ip address does exist in our list of client ip addresses
-  // we want to make sure that they cannot make a request within 100 ms
-  // of their previous request
+  // we want to make sure that they cannot make a request within 100 ms of their previous request
   setTimeout(() => delete ipAddresses[ip], delay);
 
   if (ipAddresses[ip]) return bRes.end('too soon yo');
